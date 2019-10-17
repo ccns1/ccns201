3fd20b99aa08ab0bad333519d8ac492b2ef7d508
wjj0720 <947968273@qq.com>
2019-10-17 23:48:16 +0800

    dd

--- socket.js
+++ socket.js
@@ -1,6 +1,6 @@
-const SocketIO = require("socket.io")
+const SocketIO = require('socket.io')
 const socketIO = new SocketIO({
-  path: "/websocket"
+  path: '/websocket'
 })
 
 let userList = {
@@ -13,33 +13,40 @@ let userList = {
     this.list = this.list.filter(u => u.id !== id)
     return this
   },
-  sendAllUser(name, socket) {
+  sendAllUser(name, data) {
     this.list.forEach(({ id }) => {
       console.log('>>>>>', id)
-      socketIO.to(id).emit(name, this.list)
+      socketIO.to(id).emit(name, data)
     })
     return this
+  },
+  findName(id) {
+    return this.list.find(u => u.id === id).name
   }
 }
 
-socketIO.on("connection", function(socket) {
+socketIO.on('connection', function(socket) {
   console.log('连接加入.', socket.id)
 
-  socket.on("addUser", function(data) {
+  socket.on('addUser', function(data) {
     console.log(data.name, '加入房间')
     let user = {
       id: socket.id,
       name: data.name,
       calling: false
     }
-    userList.add(user).sendAllUser("updateUserList", socket)
-
+    userList.add(user).sendAllUser('updateUserList', userList.list)
   })
 
-  socket.on("disconnect", () => {
+  socket.on('sendMessage', ({ content }) => {
+    console.log(content)
+    userList.sendAllUser('updateMessageList', { userId: socket.id, content, user: userList.findName(socket.id) })
+  })
+
+  socket.on('disconnect', () => {
     // 断开删除
-    console.log('连接断开', socket.id);
-    userList.del(socket.id).sendAllUser("updateUserList", socket)
+    console.log('连接断开', socket.id)
+    userList.del(socket.id).sendAllUser('updateUserList', userList.list)
   })
 })
 
